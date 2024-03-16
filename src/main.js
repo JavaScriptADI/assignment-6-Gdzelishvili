03111c69bf7db9c3ea753769b7627cce4d8c51c8
Lasha <grdzelishvili.lasha@yahoo.com>
2024-03-16 10:38:30 +0400

    figuring out fore each loop

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 function getCities() {
     const cities = [];
-
     for (let i = 0; i < 50; i++) {
         cities.push({
             name: `City ${i}`,
             population: Math.floor((Math.random() * 9000000) + 50000)
         })
+        
     }
     return cities;
 }
+
+// 1. მოცემულია ცხრილი, რომელშიც დატა უნდა შეავსოთ getCities ფუნქციის გამოყენებით.
+
+// ეს კოდი უნდა ჩაჯდეს ცხრილის ტანში თითოეული ქალაქისთვის:
+
+// `<tr>
+// <td>${name}</td>
+// <td>${population}</td>
+// </tr>`
+
+
+const citiesArray = getCities();
+
+const tbody = document.querySelector('#cities-table tbody');
+console.log();
+
+citiesArray.forEach(city1 => {
+    let row = document.createElement("tr");
+
+    row.innerHTML = (`<td>${city1.city}</td>
+    <td>${city1.population}</td>`);
+
+    tbody.appendChild(row);
+});
